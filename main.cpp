19226db5d6e61f94d741d74d47e3dd0352bd48a0
nipradha <nirvit.p@gmail.com>
2023-01-25 18:35:13 -0800

    hello world again

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿// main.cpp : Defines the entry point for the application.
+
+//
+
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World." << endl;
+	return 0;
+}
